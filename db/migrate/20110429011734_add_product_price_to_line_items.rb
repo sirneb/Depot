3d494d72ebc2f47d6d2626a7ba1c2add766e02e1
Frank <frankkwok@gmail.com>
2011-04-28 23:14:45 -0400

    so messed up

--- db/migrate/20110429011734_add_product_price_to_line_items.rb
+++ db/migrate/20110429011734_add_product_price_to_line_items.rb
@@ -1,8 +1,12 @@
 class AddProductPriceToLineItems < ActiveRecord::Migration
   def self.up
-    add_column :line_items, :price, :decimal, :precision => 8, :scale => 2
+    add_column :line_items, :price, :decimal, :precision => 8, :scale => 2,
+            :default => 0
 
-
+    LineItem.all.each do |item|
+      item[:price] = item.product.price * item.quantity
+      item.save
+    end
   end
 
   def self.down
