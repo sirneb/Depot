1aa565930f6aca8a3034bbb71873f3b2e8850cf0
Frank <frankkwok@gmail.com>
2011-05-01 17:17:47 -0400

    Added some mailing features

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,27 @@
+class Notifier < ActionMailer::Base
+  default :from => 'Sam Ruby <depot@example.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifier.order_received.subject
+  #
+  def order_received(order)
+    @order = order
+
+    mail :to => order.email, :subject => 'Pragmatic Store Order Confirmation'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifier.order_shipped.subject
+  #
+  def order_shipped(order)
+    @order = order
+    @is_email = true
+
+    mail :to => order.email, :subject => "Pragmatic Store Order Number #{order.id} Shipped"
+    @is_email = false
+  end
+end
